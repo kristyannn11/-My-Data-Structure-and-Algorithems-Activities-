a574c96415116d6e0d7ee39c835666c61b313265
kristyannn11 <antiojochristianjhay@gmail.com>
2024-10-10 23:22:21 +0800

    Add files via upload

---
+++ Arrayinitializationandprinting.java
@@ -0,0 +1,18 @@
+public class Static {
+    public static void main(String[] args) {
+        // Array initialization
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Printing the array
+        System.out.println("Array elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
+
+
+
+
+
+
+
